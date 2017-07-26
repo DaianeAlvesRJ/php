0badeaab1d68d0543dd02f02630b21004b8beb0a
Daiane <dsalves@id.uff.br>
2017-07-26 13:26:43 -0300

    Todas as tabuadas em um único arquivo

---
+++ tabuada.php
@@ -0,0 +1,59 @@
+<?php
+
+function tabuadaVariasLinhas($tabuada){
+    return "$tabuada x 1 = ".($tabuada * 1)."\n".
+        "$tabuada x 2 = ".($tabuada * 2)."\n".
+        "$tabuada  x 3 = ".($tabuada * 3)."\n".
+        "$tabuada  x 4 = ".($tabuada * 4)."\n".
+        "$tabuada  x 5 = ".($tabuada * 5)."\n".
+        "$tabuada  x 6 = ".($tabuada * 6)."\n".
+        "$tabuada  x 7 = ".($tabuada * 7)."\n".
+        "$tabuada  x 8 = ".($tabuada * 8)."\n".
+        "$tabuada  x 9 = ".($tabuada * 9)."\n".
+        "$tabuada  x 10 = ".($tabuada * 10)."\n";
+}
+//echo tabuadaVariasLinhas(6);
+
+function tabuadaArray($tabuada){
+    $linhas[] = "$tabuada x 1 = ".($tabuada * 1);
+    $linhas[] = "$tabuada x 2 = ".($tabuada * 2);
+    $linhas[] = "$tabuada x 3 = ".($tabuada * 3);
+    $linhas[] = "$tabuada x 4 = ".($tabuada * 4);
+    $linhas[] = "$tabuada x 5 = ".($tabuada * 5);
+    $linhas[] = "$tabuada x 6 = ".($tabuada * 6);
+    $linhas[] = "$tabuada x 7 = ".($tabuada * 7);
+    $linhas[] = "$tabuada x 8 = ".($tabuada * 8);
+    $linhas[] = "$tabuada x 9 = ".($tabuada * 9);
+    $linhas[] = "$tabuada x 10 = ".($tabuada * 10);
+return implode("\n", $linhas);
+
+}
+
+//echo tabuadaArray(5);
+
+function tabuadaFor($tabuada){
+for ($numero = 1; $numero <= 10; $numero++){
+    $linhas[] = "$tabuada x $numero = ".($tabuada *$numero);
+    }
+return implode("\n", $linhas);
+}
+
+//echo tabuadaFor(4);
+
+function tabuadaDoWhile($tabuada){
+    $numero = 1;
+    do{
+        $linhas[] = "$tabuada x $numero = " . ($tabuada * $numero);
+        $numero++;
+    }while($numero <=10);
+    return implode("\n", $linhas);
+}
+echo tabuadaDoWhile(8);
+
+function TabuadaForEach($tabuada){
+    $lista = [1,2,3,4,5,6,7,8,9,10];
+    foreach ($lista as $numero) {
+        $linhas[] = "$tabuada x $numero = " . ($tabuada * $numero);
+    }
+}
+//echo TabuadaForEach(2); //NÃO IMPRIMIU NADA!!!!
